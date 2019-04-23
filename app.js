a1976b2305bc9554ce9d875471b61df00cf95465
sunhaiyang <sunhaiyang@100tal.com>
2019-04-23 11:31:06 +0800

    session相关

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const handleBlogRouter = require('./src/router/blog.js')
 const handleUserRouter = require('./src/router/user.js')
 const qs = require('querystring')
 
+let SESSION_DATA = {}
+
 const getPostData = req=>{
     const promise = new Promise((resolve,reject)=>{
         if(req.method !== 'POST'){
@@ -28,6 +30,11 @@ const getPostData = req=>{
     })
     return promise
 }
+const getCookieExpires = ()=>{
+    const d = new Date()
+    d.setTime(d.getTime() + 24*60*60*1000)
+    return d.toGMTString()
+}
 
 const serverHandler = (req,res)=>{
     //设置返回格式
@@ -39,6 +46,39 @@ const serverHandler = (req,res)=>{
 
     //解析query
     req.query  = qs.parse(url.split('?')[1])
+
+    //解析cookie
+    req.cookie = {}
+    const cookieStr = req.headers.cookie || '';
+    cookieStr.split(';').forEach(item => {
+        if(!item) return;
+
+        const arr = item.split('=');
+        const key = arr[0];
+        const val = arr[1];
+
+        req.cookie[key] = val;
+    });
+
+    //解析session
+    let needSetCookie = false;
+    let userId = req.cookie.userid;
+
+    //首次登陆，cookie中没有userId，随机起一个userId，Session_DATA上此userId挂在为空
+    //去数据库查询登陆，通过设置req.session设置SESSION_DATA[userId]
+    //将userId返回给浏览器
+    //登陆完以后再进来 因为cookie中已经有userId了,所以直接去SESSION_DATA去找对应的userId，拿用户信息
+    if(userId){
+        if(!SESSION_DATA[userId]){
+            SESSION_DATA[userId] = {};
+        }
+    }else{
+        needSetCookie = true;
+        userId = `${Date.now()}_${Math.random()}` //保证不重复
+        SESSION_DATA[userId] = {};
+    }
+    req.session = SESSION_DATA[userId]; //session是个对象
+
     getPostData(req).then(postData=>{
         req.body = postData
 
@@ -46,6 +86,11 @@ const serverHandler = (req,res)=>{
         const blogResult = handleBlogRouter(req,res )
         if(blogResult){
             blogResult.then(blogData=>{
+                if(needSetCookie){
+                    //cookie设置httpOnly以后，前端就不能改了
+                    res.setHeader('Set-Cookie', `userid=${userId};path=/;httpOnly;expires=${getCookieExpires()}`)
+                }
+
                 res.end(JSON.stringify(blogData))
             })
             return;
@@ -55,6 +100,10 @@ const serverHandler = (req,res)=>{
         const userResult = handleUserRouter(req,res )
         if(userResult){
             userResult.then(userData=>{
+                if(needSetCookie){
+                    //cookie设置httpOnly以后，前端就不能改了
+                    res.setHeader('Set-Cookie', `userid=${userId};path=/;httpOnly;expires=${getCookieExpires()}`)
+                }
                 res.end(JSON.stringify(userData))
             })
             return;
--- src/router/user.js
+++ src/router/user.js
@@ -1,20 +1,39 @@
-const {loginCheck} = require('../controller/user')
+const {login} = require('../controller/user')
 const {SuccessModel,ErrorModel} = require('../model/resModel.js')
 
 const handleUserRouter = (req,res)=>{
     const method = req.method;
 
-    if(method == 'POST' && req.path == '/api/user/login'){
-        const {username , password} = req.body;
-        const result = loginCheck(username, password);
+    if(method == 'GET' && req.path == '/api/user/login'){
+        //const {username , password} = req.body;
+        const {username, password} = req.query;
+        const result = login(username, password);
 
         return result.then(data=>{
             if(data.username){
+                req.session.username = data.username;
+                req.session.realname = data.realname;
+
                 return new SuccessModel('登陆成功') 
             }else{
                 return new ErrorModel('登陆失败')
             }
         })
+    }
+
+    //验证登陆状态的一个接口
+    if(method == 'GET' && req.path == '/api/user/login-test'){
+        console.log('req.session',req.session)
+        
+        if(req.session.username){
+            return Promise.resolve(new SuccessModel({
+                session : req.session
+            }))
+        }else{
+            return Promise.resolve(
+                new ErrorModel('尚未登陆')
+            ) 
+        }
     }
 }
 
